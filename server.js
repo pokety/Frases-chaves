8bf3de8bdef30b07f97326d5801eba6626ca02dd
Claudio Amilton <pokety@gmail.com>
2022-09-09 20:49:38 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const path = require('path');
 const bodyparse = require('body-parser');
 const api = require('./public/src/api.json');
 const fs = require('fs-extra');
+const cors=require('cors');
 
 const urlencoder = bodyparse.urlencoded({ extended: false });
 
+app.use(cors())
 app.use(express.static('public'));
 app.get('/api', (req, res) => {
   return res.json(api);
