8e1f3154ca56eefa151cea43de0d6c02fe28f276
pokety <pokety@gmail.com>
2022-09-14 09:10:38 -0300

    1 file modified, 2 added, 2 deleted

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyparse = require('body-parser');
-const api = require('./public/src/api.json');
+const api = require('./api.json');
 const fs = require('fs-extra');
 const cors = require('cors');
 
 const urlencoder = bodyparse.urlencoded({ extended: false });
 
 app.use(cors());
-app.use(express.static('public'));
 app.get('/api', (req, res) => {
   return res.json(api);
 });
@@ -18,14 +17,14 @@ app.get('/ramdon', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+  res.sendFile(path.join(__dirname, './index.html'));
 });
 
 app.post('/', urlencoder, (req, res) => {
   const frase1 = req.body.frase;
   const autor1 = req.body.autor;
   api.push({ frase: frase1, autor: autor1 });
-  fs.writeJson('./public/src/api.json', api)
+  fs.writeJson('./api.json', api)
     .then(() => console.log('sucesso'))
     .catch((err) => console.log(err));
   console.log(api);
