8e8ed7cdb88e84b353774b0173fe15a569dc4a29
pokety <pokety@gmail.com>
2022-09-14 09:18:56 -0300

    1 file modified

--- app.js
+++ app.js
@@ -11,15 +11,15 @@ const urlencoder = bodyparse.urlencoded({ extended: false });
 app.get('/api', (req, res) => {
   return res.json(api);
 });
-app.get('/ramdon', (req, res) => {
+app.get('/', (req, res) => {
   return res.json(api[Math.floor(Math.random() * 29)]);
 });
 
-app.get('/', (req, res) => {
+app.get('/admin', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
 });
 
-app.post('/', urlencoder, (req, res) => {
+app.post('/admin', urlencoder, (req, res) => {
   const frase1 = req.body.frase;
   const autor1 = req.body.autor;
   api.push({ frase: frase1, autor: autor1 });
